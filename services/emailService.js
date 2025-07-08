32ddc66181fd655bab387e9e112e09943b8ef07c
KudzaiMunyukwa95 <kaymunyukwa@gmail.com>
2025-07-08 04:22:02 +0200

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -3,10 +3,10 @@ const handlebars = require('handlebars');
 
 class EmailService {
   constructor() {
-    this.transporter = nodemailer.createTransporter({
+    this.transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: parseInt(process.env.EMAIL_PORT) || 587,
-      secure: false, // true for 465, false for other ports
+      secure: parseInt(process.env.EMAIL_PORT) === 465, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
